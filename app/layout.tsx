8142c887beca85c037d42597aa4c0b21b1bb2b7c
Daedreamer7 <Denise@conceptsplus.net>
2025-01-27 19:41:51 -0600

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,8 +2,8 @@ import type { Metadata } from 'next'
 import './globals.css'
 
 export const metadata: Metadata = {
-  title: 'v0 App',
-  description: 'Created with v0',
+  title: 'concepts on concepts',
+  description: 'widget show-case',
 }
 
 export default function RootLayout({
